a8e41e0b9134b84391992de0481bb66af3128b8a
witsuta onampai <witsuta.on@ku.th>
2021-03-06 15:50:43 +0700

    initial project bankaccount-api

---
+++ src/main/java/th/ac/ku/bankaccount/model/BankAccount.java
@@ -0,0 +1,51 @@
+package th.ac.ku.bankaccount.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class BankAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int customerId;
+    private String type;
+    private double balance;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
